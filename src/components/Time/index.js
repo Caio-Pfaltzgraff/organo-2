3a12431443ec598852f20c36a1aecc14b5bf3875
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-14 15:41:36 -0300

    funcionalidade de deletar

---
+++ src/components/Time/index.js
@@ -0,0 +1,20 @@
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+const Time = (props) => {
+const css = { backgroundColor: props.corSecundaria, backgroundImage: 'url(/images/fundo.png)' }
+
+    return (
+        (props.colaboradores.length > 0) && <section className='time' style={css}>
+            <input value={props.corPrimaria} type='color' className='input-cor' />
+            <h3 style={{borderColor : props.corPrimaria}}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => {
+                    return <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={props.aoDeletar}/>
+                })}
+            </div>
+        </section>
+    )
+}
+
+export default Time
